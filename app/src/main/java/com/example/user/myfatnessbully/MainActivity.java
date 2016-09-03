d409ff50cfbaf405d2b0c2e992717ed5f25b1080
davidmorrison1981 <david.morrison@geminises.com>
2016-09-03 16:47:59 +0100

    yeah just remember about git commits

---
+++ app/src/main/java/com/example/user/myfatnessbully/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.user.myfatnessbully;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button mSaveUsername;
+    EditText mUsername;
+    TextView mSavedText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mUsername = (EditText)findViewById(R.id.username);
+        mSaveUsername = (Button)findViewById(R.id.saveUsername);
+        mSavedText = (TextView)findViewById(R.id.saved_text);
+        mSavedText.setVisibility(View.INVISIBLE);
+
+        String savedText = SavedTextPreferences.getStoredText(this);
+        if(savedText != null && !savedText.isEmpty()){
+            mSaveUsername.setVisibility(View.INVISIBLE);
+            mUsername.setVisibility(View.INVISIBLE);
+            mSavedText.setText(savedText);
+            mSavedText.setVisibility(View.VISIBLE);
+        }
+
+        mSaveUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textToSave = mUsername.getText().toString();
+                mSaveUsername.setVisibility(View.INVISIBLE);
+                mUsername.setVisibility(View.INVISIBLE);
+                mSavedText.setVisibility(View.VISIBLE);
+                mSavedText.setText(textToSave);
+                Context context = v.getContext();
+                SavedTextPreferences.setStoredText(context, textToSave);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/user/myfatnessbully/SavedTextPreferences.java
@@ -0,0 +1,34 @@
+package com.example.user.myfatnessbully;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by user on 03/09/2016.
+ */
+
+public class SavedTextPreferences {
+
+        private static final String PREF_SAVEDTEXT ="savedText";
+
+        public static void setStoredText(Context context, String text){
+            SharedPreferences sharedPreferences =
+                    PreferenceManager.getDefaultSharedPreferences(context);
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(PREF_SAVEDTEXT,text);
+            editor.apply();
+
+        }
+
+        public static String getStoredText(Context context){
+            SharedPreferences sharedPreferences =
+                    PreferenceManager.getDefaultSharedPreferences(context);
+
+            String text = sharedPreferences.getString(PREF_SAVEDTEXT,null);
+            return text;
+        }
+
+    }
+
