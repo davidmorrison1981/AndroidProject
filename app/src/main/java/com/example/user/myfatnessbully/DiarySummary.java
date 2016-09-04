67f3dc2ee3adaa3de5cc5f065dc9e1ca814d72eb
davidmorrison1981 <david.morrison@geminises.com>
2016-09-04 15:44:14 +0100

    fixed summary screen base layout by removing NoActionBar

--- app/src/main/java/com/example/user/myfatnessbully/DiarySummary.java
+++ app/src/main/java/com/example/user/myfatnessbully/DiarySummary.java
@@ -2,6 +2,7 @@ package com.example.user.myfatnessbully;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by user on 04/09/2016.
@@ -10,7 +11,8 @@ public class DiarySummary extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Diary Summary:", "OnCreate started");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.summary_page);
+        setContentView(R.layout.diary_main);
     }
 }
