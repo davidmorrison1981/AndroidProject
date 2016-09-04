3b373e50ecd1e28fd793000c8650680268e3f8bf
davidmorrison1981 <david.morrison@geminises.com>
2016-09-04 16:18:23 +0100

    added classes for each of the activities

---
+++ app/src/main/java/com/example/user/myfatnessbully/EditDiary.java
@@ -0,0 +1,21 @@
+package com.example.user.myfatnessbully;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by user on 04/09/2016.
+ */
+public class EditDiary extends AppCompatActivity {
+
+    public class DiarySummary extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.diary_edit);
+        }
+    }
+
+}
