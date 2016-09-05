49f20cd67afa7a7348eb38de94c5fe8a9485794b
davidmorrison1981 <david.morrison@geminises.com>
2016-09-05 12:18:16 +0100

    date popup fixed

--- app/src/main/java/com/example/user/myfatnessbully/EditDiary.java
+++ app/src/main/java/com/example/user/myfatnessbully/EditDiary.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.DatePicker;
-
 import java.util.Calendar;
 
 /**
@@ -29,6 +28,11 @@ public class EditDiary extends AppCompatActivity {
             return new DatePickerDialog(getActivity(), this, year, month, day);
         }
 
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day){
+
+
+        }
     }
 
     @Override
