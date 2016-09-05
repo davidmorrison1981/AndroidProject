16bde5603c37e8a9db8cb69d762ffd0c29033145
davidmorrison1981 <david.morrison@geminises.com>
2016-09-05 16:35:17 +0100

    adding to sql handler

--- app/src/main/java/com/example/user/myfatnessbully/SqlHandler.java
+++ app/src/main/java/com/example/user/myfatnessbully/SqlHandler.java
@@ -17,24 +17,24 @@ public class SqlHandler extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "foodDiary";
 
     // Contacts table name
-    private static final String TABLE_CONTACTS = "foodEntry";
+    private static final String TABLE_ENTRIES = "foodEntry";
 
     // Contacts Table Columns names
     private static final String KEY_ID = "id";
-    private static final String KEY_NAME = "food_type";
+    private static final String KEY_FOOD = "food_type";
     private static final String KEY_CALORIES = "calories";
     private static final String KEY_DATE = "date";
 
-    public DatabaseHandler(Context context) {
+    public SqlHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     // Creating Tables
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
-                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
-                + KEY_PH_NO + " TEXT" + ")";
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_ENTRIES + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_FOOD + " TEXT," +
+                KEY_CALORIES + "INT," + KEY_DATE + " TEXT" + ")";
         db.execSQL(CREATE_CONTACTS_TABLE);
     }
 
@@ -42,10 +42,37 @@ public class SqlHandler extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop older table if existed
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ENTRIES);
 
         // Create tables again
         onCreate(db);
     }
+
+        // Adding new row entry
+    public void addFoodEntry(FoodEntry foodEntry){
+    }
+
+    // Getting single contact
+    public FoodEntry getFoodEntry(int id) {
+    }
+
+    // Getting All Contacts
+    public List<FoodEntry> getAllFoodEntries() {
+    }
+
+    // Getting food entries Count
+    public int getFoodEntriesCount() {
+    }
+
+    // Updating single contact
+    public int updateFoodEntry(foodEntry contact) {
+    }
+
+    // Deleting single contact
+    public void deleteContact(Contact contact) {
+
+    }
+}
+
 
 
